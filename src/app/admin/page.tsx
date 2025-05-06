ffb529e7fd4bf8a61fa96aa7593a39834ac1bc4d
AkhilchandranR <akhilchandran771@gmail.com>
2025-05-06 18:55:52 +0530

    datbase queried

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,47 @@
-export default function AdminDashboard() {
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { prisma } from "../db/db";
+
+async function getSalesData() {
+    const data = await prisma.order.aggregate({
+        _sum: { pricePaidInCents: true },
+        _count: true
+    });
+
+    return {
+        amount: (data._sum.pricePaidInCents || 0)/100,
+        numberOfSales: data._count
+    }
+}
+
+export default async function AdminDashboard() {
+    const salesData = await getSalesData();
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            
+            <DashboardCard 
+                title="Sales" 
+                subtitle={salesData.numberOfSales} 
+                body={salesData.amount} 
+            />
         </div>
     )
-};   
+};
+
+type DashboardCardProps = {
+    title: string;
+    subtitle: string;
+    body: string
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+    return (
+            <Card>
+                <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                    <CardDescription>{subtitle}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    {body}
+                </CardContent>
+            </Card>
+    )
+}
